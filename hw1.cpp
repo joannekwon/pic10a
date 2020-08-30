49c96b92410b94b4f6fd19760418b190a0c3bcdf
Joanne Kwon <66094691+joannekwon@users.noreply.github.com>
2020-08-30 10:48:21 -0400

    Add files via upload

---
+++ hw1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "              ___  _____ " << endl;
+	cout << "            .'/,-Y\"     \"~-.            " << endl;
+	cout << "            l.Y             ^." << endl;
+	cout << "            /\\               _\\_" << endl;
+	cout << "           i            ___/\"   \"\\" << endl;
+	cout << "           |          /\"   \"\\  o  !" << endl;
+	cout << "           l         l     o !__./ " << endl;
+	cout << "            \\ _  _    \\.___./   \"~\\     " << endl;
+	cout << "             X \\/ \\           ___./ " << endl;
+	cout << "            < \\ ___.   _..--~~\"   ~\`-.  " << endl;
+	cout << "             \` Z,--   /               \\ " << endl;
+	cout << "               \\__.  <   /       ______>" << endl;
+	cout << "                 \\   l  /-----~~\" /     " << endl;
+	cout << "                  Y   \\          / " << endl;
+	cout << "                  |    \"x______.^       " << endl;
+	cout << "                  |           \\" << endl;
+	cout << "                  j            Y" << endl;
+
+	system("pause");
+	return 0;
+}
